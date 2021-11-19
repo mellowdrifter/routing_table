affc3d2eb2e2fef2275500191b989f3272b24e7b
Darren O'Connor <mellow.drifter@gmail.com>
2021-11-19 20:40:14 +0000

    somehow this actually works

--- routing_table.go
+++ routing_table.go
@@ -35,30 +35,25 @@ func (r *Rib) Insert(prefix netaddr.IPPrefix) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	fmt.Printf("inserting %s\n", prefix.String())
 	currentNode := r.root
 	addr := prefix.IP().As4()
 	mask := prefix.Bits()
-	var bitCount uint8
+	bitCount := uint8(1)
 	// <3 because we really don't care about the last octet as we won't store anything > 24
 	for i := 0; i < 3; i++ {
 		// TODO: We never have < /8 either, so the first node should really be a decimal!
 		bits := intToBinBitwise(addr[i])
-		//fmt.Printf("Inserting octet %d\n", addr[i])
 		for _, bit := range bits {
-			//fmt.Printf("Inserting bit %d\n", bit)
 			if currentNode.children[bit] == nil {
-				//fmt.Println("This bit does not exist, so adding")
 				currentNode.children[bit] = &node{}
 			}
 			currentNode = currentNode.children[bit]
-			bitCount++
 			if bitCount == mask {
 				currentNode.mask = mask
 				currentNode.prefix = &prefix
-				//fmt.Printf("Reached %d bits, so stopping\n", bitCount)
 				return
 			}
+			bitCount++
 		}
 	}
 }
@@ -68,21 +63,31 @@ func (r *Rib) Search(ip netaddr.IP) *netaddr.IPPrefix {
 	r.mu.RLock()
 	defer r.mu.RUnlock()
 
+	lpm := &netaddr.IPPrefix{}
+
 	fmt.Printf("Finding IP address: %s\n", ip.String())
 	currentNode := r.root
 	addr := ip.As4()
+	bitCount := uint8(1)
 	// <3 because we really don't care about the last octet as we won't store anything > 24
 	for i := 0; i < 3; i++ {
 		bits := intToBinBitwise(addr[i])
-		fmt.Printf("Finding octet %d\n", addr[i])
 		for _, bit := range bits {
 			if currentNode.children[bit] != nil {
+				// save the current best path
 				currentNode = currentNode.children[bit]
-			}
-			if currentNode.prefix != nil {
-				return currentNode.prefix
+				if currentNode.prefix != nil {
+					//fmt.Printf("lpm is currently %v\n", currentNode.prefix.String())
+					lpm = currentNode.prefix
+				}
+				bitCount++
+			} else {
+				break
 			}
 		}
+	}
+	if lpm.Contains(ip) {
+		return lpm
 	}
 	return nil
 }
